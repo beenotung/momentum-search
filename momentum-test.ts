529ad90ff33dfc116d3a257f50ca3f042b56fce1
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-08-02 23:34:59 +0800

    feat: add momentum-based searching algorithm with tune() step function and auto_tune() iteration function init: setup project skeleton with "install-ts" dev script

---
+++ momentum-test.ts
@@ -0,0 +1,65 @@
+import { create_momentum } from './momentum'
+
+let sample_size = 1000
+let dataset: { x: number[]; y: number }[] = []
+
+function loadDataset() {
+  let c0 = Math.PI
+  let c1 = Math.E
+  let c2 = Math.sqrt(2)
+  let c3 = 12.5
+
+  for (let i = 0; i < sample_size; i++) {
+    let x1 = Math.random() * 2 - 1
+    let x2 = Math.random() * 2 - 1
+    let x3 = Math.random() * 2 - 1
+    let y = c0 + c1 * x1 + c2 * x2 + c3 * x3
+    dataset.push({ x: [x1, x2, x3], y })
+  }
+}
+
+loadDataset()
+
+let parameters = create_momentum({ n: 4 })
+let coefficients = parameters.values
+
+function predict(x: number[]) {
+  return (
+    coefficients[0] * x[0] +
+    coefficients[1] * x[1] +
+    coefficients[2] * x[2] +
+    coefficients[3]
+  )
+}
+
+function benchmark() {
+  let mse = 0
+  for (let data of dataset) {
+    let error = predict(data.x) - data.y
+    mse += error * error
+  }
+  mse /= dataset.length
+  let error = Math.sqrt(mse)
+  return error
+}
+
+function f(x: number): string {
+  return x.toPrecision(3)
+}
+
+parameters.auto_tune({
+  loss_fn: benchmark,
+  min_loss: 1 / 1000,
+  min_step: 1 / 1000,
+  iterate_callback: options => {
+    let { epoch, tuned } = options
+    let error = benchmark()
+    console.log(
+      `epoch=${epoch}, tuned=${f(tuned)}, error=${f(error)}, coefficients=${
+        parameters.values
+      }`,
+    )
+  },
+})
+
+console.log(parameters.values)
---
+++ momentum.ts
@@ -0,0 +1,135 @@
+let default_initial_step = 1048576
+
+export function create_momentum(options: { n: number; initial_step?: number }) {
+  let n = options.n
+
+  let values: number[] = new Array(n).fill(0)
+  let momentums: number[] = new Array(n).fill(
+    options.initial_step || default_initial_step,
+  )
+
+  /* internal variables of tune() */
+  let tuned = 0
+  let base_loss = NaN
+
+  /**
+   * @description a step function to tune the values based on momentums.
+   * The momentums are auto adjusted up/down based on the sampled gradient.
+   */
+  function tune(loss_fn: (values: number[]) => number): void {
+    base_loss = loss_fn(values)
+    tuned = 0
+    for (let i = 0; i < n; i++) {
+      let base_value = values[i]
+
+      for (;;) {
+        let momentum = momentums[i]
+        if (momentum == 0) {
+          values[i] = base_value
+          break
+        }
+
+        values[i] = base_value + momentum
+        let forward_loss = loss_fn(values)
+
+        if (forward_loss < base_loss) {
+          /* forward is better */
+          base_loss = forward_loss
+          tuned += Math.abs(momentum)
+          momentums[i] *= 1.5
+          break
+        }
+
+        values[i] = base_value - momentum
+        let backward_loss = loss_fn(values)
+        if (backward_loss < base_loss) {
+          /* backward is better */
+          base_loss = backward_loss
+          tuned += Math.abs(momentum)
+          momentums[i] *= -1
+          break
+        }
+
+        /* both are not good */
+        momentums[i] /= 2
+        continue
+      }
+    }
+  }
+
+  /**
+   * @description auto call tune() in loop,
+   * until the tuned amount is below min_step,
+   * or the loss_fn() output is below min_loss
+   * */
+  function auto_tune(options: {
+    /**
+     * @description loss function to be minimized.
+     * */
+    loss_fn: (values: number[]) => number
+    /**
+     * @description set the initial momentum to be this value
+     * @default 1048576
+     */
+    initial_step?: number
+
+    /**
+     * @description stopping condition when the tuned amount is below this value
+     * @default 0
+     */
+    min_step?: number
+
+    /**
+     * @description stopping condition when the loss is below this value
+     * @default 0
+     */
+    min_loss?: number
+
+    /**
+     * @description optional post-epoch callback for progress report
+     */
+    iterate_callback?: (options: {
+      epoch: number
+      tuned: number
+      loss: number
+      values: number[]
+      momentums: number[]
+    }) => void
+  }) {
+    let initial_step = options.initial_step || default_initial_step
+    let min_step = options.min_step || 0
+    let min_loss = options.min_loss || 0
+    let { loss_fn, iterate_callback } = options
+
+    momentums.fill(initial_step)
+
+    if (iterate_callback) {
+      let epoch = 0
+      for (;;) {
+        epoch++
+        tune(loss_fn)
+        iterate_callback({ epoch, tuned, loss: base_loss, values, momentums })
+        if (tuned <= min_step || base_loss <= min_loss) {
+          break
+        }
+      }
+    } else {
+      for (;;) {
+        tune(loss_fn)
+        if (tuned <= min_step || base_loss <= min_loss) {
+          break
+        }
+      }
+    }
+  }
+
+  return {
+    values,
+    momentums,
+    tune,
+    auto_tune,
+    get last_tune_stats() {
+      return { tuned, loss: base_loss }
+    },
+  }
+}
